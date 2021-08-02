20e5fdf8b3f7eac71c2cc2117358d22e451c473b
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-08-01 22:45:11 -0400

    Vue 3 Modal

---
+++ app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Karla:wght@300;400&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Karla", sans-serif;
+}
+
+button {
+  padding: 20px 30px;
+  border: none;
+  font-size: 16px;
+  background-color: crimson;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
---
+++ app/src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <Modal @close="toggleModal" :modalActive="modalActive">
+      <div class="modal-content">
+        <h1>This is a Modal Header</h1>
+        <p>This is a modal message</p>
+      </div>
+    </Modal>
+    <button @click="toggleModal" type="button">Open Modal</button>
+  </div>
+</template>
+
+<script>
+import Modal from "@/components/Modal.vue";
+import { ref } from "vue";
+export default {
+  name: "Home",
+  components: {
+    Modal,
+  },
+  setup() {
+    const modalActive = ref(false);
+
+    const toggleModal = () => {
+      modalActive.value = !modalActive.value;
+    };
+
+    return { modalActive, toggleModal };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  background-color: rgba(0, 176, 234, 0.5);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .modal-content {
+    display: flex;
+    flex-direction: column;
+
+    h1,
+    p {
+      margin-bottom: 16px;
+    }
+
+    h1 {
+      font-size: 32px;
+    }
+
+    p {
+      font-size: 18px;
+    }
+  }
+}
+</style>
